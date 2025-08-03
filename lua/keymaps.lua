b193a222ff0a4c1e437b24d6d52db6d966eaf17d
leopalatto20 <A01665462@tec.mx>
2025-08-03 16:32:52 -0600

    keymap para find todos

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -30,4 +30,6 @@ vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope 
 vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 
+vim.keymap.set('n', '<leader>ft', ':TodoTelescope<CR>', opts)
+
 vim.cmd 'colorscheme catppuccin-mocha'
