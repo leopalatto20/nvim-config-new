2b493d01ece12e5f94d2565aaa259be45cfe0944
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2026-02-09 20:57:15 -0600

    Cambio de tema e implementar python

--- lua/plugins/pythonenv.lua
+++ lua/plugins/pythonenv.lua
@@ -4,7 +4,7 @@ return {
     cmd = 'VenvSelect',
     opts = {
       options = {
-        notify_user_on_venv_activation = true,
+        notify_user_on_venv_activation = false,
       },
     },
     ft = 'python',
